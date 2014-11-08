31827963fed9f05d9abb76e6ef72d9000e2578f2
Edward L Platt <ed@elplatt.com>
2014-11-08 16:05:53 -0500

    Add jquery, data, main js file.

---
+++ js/data.js
@@ -0,0 +1,18 @@
+data = [
+    {
+        "name": "Hackerspace OS"
+        , "features": "Events, projects."
+        , "license": "Open source, unknown."
+        , "development": "Metalab fork is active, but focused on their local instance."
+        , "stack": "Python, django."
+        , "documentation": "None."
+        , "installation": "Undocumented dependencies. Runtime errors on Ubuntu 14.04 LTS."
+        , "usability": ""
+        , "security": ""
+        , "international": "Built-in through django. Strings in German."
+        , "dataformat": ""
+        , "api": ""
+        , "auth": ""
+        , "custom": ""
+    }
+]
